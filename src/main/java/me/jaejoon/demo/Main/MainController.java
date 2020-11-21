a0ca60cf1a75bca39968524741728f71ca648e2a
Jae Joon <dkansk924@naver.com>
2020-11-21 20:04:32 +0900

    현재 인증된 사용자정보 참조

---
+++ src/main/java/me/jaejoon/demo/Main/MainController.java
@@ -0,0 +1,19 @@
+package me.jaejoon.demo.Main;
+
+import me.jaejoon.demo.account.CurrentUser;
+import me.jaejoon.demo.domain.Account;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+
+    @GetMapping("/")
+    public String mainHome(Model model , @CurrentUser Account account){
+        if(account != null) {
+            model.addAttribute(account);
+        }
+        return "index";
+    }
+}
---
+++ src/main/java/me/jaejoon/demo/account/UserAccount.java
@@ -0,0 +1,21 @@
+package me.jaejoon.demo.account;
+
+import me.jaejoon.demo.domain.Account;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+
+import java.util.List;
+
+public class UserAccount extends User {
+
+    private final Account account;
+
+    public UserAccount(Account account) {
+        super(account.getNickname() ,account.getPassword(), List.of(new SimpleGrantedAuthority("ROLE_USER")));
+        this.account = account;
+    }
+
+    public Account getAccount() {
+        return account;
+    }
+}
