01c0546a972169aecab131f96d61e4d94531cd3a
Jae Joon <dkansk924@naver.com>
2020-12-09 23:01:33 +0900

    DescriptionSetting   get/post 요청 test 코드 작성

--- src/test/java/me/jaejoon/demo/study/StudySettingControllerTest.java
+++ src/test/java/me/jaejoon/demo/study/StudySettingControllerTest.java
@@ -2,6 +2,7 @@ package me.jaejoon.demo.study;
 
 import me.jaejoon.demo.WithAccountAndStudyPage;
 import me.jaejoon.demo.account.CurrentUser;
+import me.jaejoon.demo.domain.Study;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -29,16 +30,40 @@ class StudySettingControllerTest {
     @Autowired
     MockMvc mockMvc;
 
+    @Autowired
+    StudyRepository studyRepository;
+
     @Test
-    @DisplayName("스터디 설정 page 보기")
-    @WithAccountAndStudyPage(value ="kjj924",title = "봄싹스터디",path = "test")
-    void viewStudySetting() throws Exception {
+    @DisplayName("스터디 소개 설정 페이지 보기")
+    @WithAccountAndStudyPage(value ="kjj924",title ="봄싹스터디",path = "test")
+    void viewDescriptionSetting() throws Exception {
         String path = URLEncoder.encode("test", StandardCharsets.UTF_8);
+
         mockMvc.perform(get("/study/"+ path +"/settings/description"))
                 .andExpect(status().isOk())
                 .andExpect(model().attributeExists("account"))
                 .andExpect(model().attributeExists("study"))
                 .andExpect(model().attributeExists("studyDescriptionForm"));
+    }
+
+    @Test
+    @DisplayName("스터디 소개 변경")
+    @WithAccountAndStudyPage(value ="kjj924",title ="봄싹스터디",path = "test")
+    void editDescription() throws Exception{
+        String path = URLEncoder.encode("test", StandardCharsets.UTF_8);
+        mockMvc.perform(post("/study/"+ path +"/settings/description")
+                .param("shortDescription","shortDescription 변경됨")
+                .param("fullDescription","fullDescription 변경됨")
+                .with(csrf()))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl("/study/"+ path +"/settings/description"))
+                .andExpect(flash().attributeExists("message"));
+
+        Study study = studyRepository.findByPath(path);
+        assertThat(study.getShortDescription()).isEqualTo("shortDescription 변경됨");
+        assertThat(study.getFullDescription()).isEqualTo("fullDescription 변경됨");
+
+
     }
 
 }
